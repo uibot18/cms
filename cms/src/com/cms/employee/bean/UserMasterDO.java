1753821ac1e870fd59d5edd7cd678c03312e3673
Jesuitsoft <jesu.itsoft@gmail.com>
2019-01-08 23:52:02 +0530

    User Master

---
+++ cms/src/com/cms/employee/bean/UserMasterDO.java
@@ -0,0 +1,162 @@
+package com.cms.employee.bean;
+
+public class UserMasterDO { 
+
+private int empId=0;
+private String firstName="";
+private String lastName="";
+private String email="";
+private String gender="Male";
+private String maritalStatus="Single";
+private int bloodGroup=0;
+private int mobile=0;
+private String dateOfBirth="";
+private int department=0;
+private int role=0;
+private int profile=0;
+private int reportingManager=0;
+private String subordinates="";
+private String panCard="";
+private String bankDetials="";
+private String epfNo="";
+private String street="";
+private String city="";
+private int state=0;
+private String zipcode="";
+private int country=0;
+
+
+public void setEmpId(int empId){
+this.empId=empId;
+}
+public int getEmpId(){
+return this.empId;
+}
+public void setFirstName(String firstName){
+this.firstName=firstName;
+}
+public String getFirstName(){
+return this.firstName;
+}
+public void setLastName(String lastName){
+this.lastName=lastName;
+}
+public String getLastName(){
+return this.lastName;
+}
+public void setEmail(String email){
+this.email=email;
+}
+public String getEmail(){
+return this.email;
+}
+public void setGender(String gender){
+this.gender=gender;
+}
+public String getGender(){
+return this.gender;
+}
+public void setMaritalStatus(String maritalStatus){
+this.maritalStatus=maritalStatus;
+}
+public String getMaritalStatus(){
+return this.maritalStatus;
+}
+public void setBloodGroup(int bloodGroup){
+this.bloodGroup=bloodGroup;
+}
+public int getBloodGroup(){
+return this.bloodGroup;
+}
+public void setMobile(int mobile){
+this.mobile=mobile;
+}
+public int getMobile(){
+return this.mobile;
+}
+public void setDateOfBirth(String dateOfBirth){
+this.dateOfBirth=dateOfBirth;
+}
+public String getDateOfBirth(){
+return this.dateOfBirth;
+}
+public void setDepartment(int department){
+this.department=department;
+}
+public int getDepartment(){
+return this.department;
+}
+public void setRole(int role){
+this.role=role;
+}
+public int getRole(){
+return this.role;
+}
+public void setProfile(int profile){
+this.profile=profile;
+}
+public int getProfile(){
+return this.profile;
+}
+public void setReportingManager(int reportingManager){
+this.reportingManager=reportingManager;
+}
+public int getReportingManager(){
+return this.reportingManager;
+}
+public void setSubordinates(String subordinates){
+this.subordinates=subordinates;
+}
+public String getSubordinates(){
+return this.subordinates;
+}
+public void setPanCard(String panCard){
+this.panCard=panCard;
+}
+public String getPanCard(){
+return this.panCard;
+}
+public void setBankDetials(String bankDetials){
+this.bankDetials=bankDetials;
+}
+public String getBankDetials(){
+return this.bankDetials;
+}
+public void setEpfNo(String epfNo){
+this.epfNo=epfNo;
+}
+public String getEpfNo(){
+return this.epfNo;
+}
+public void setStreet(String street){
+this.street=street;
+}
+public String getStreet(){
+return this.street;
+}
+public void setCity(String city){
+this.city=city;
+}
+public String getCity(){
+return this.city;
+}
+public void setState(int state){
+this.state=state;
+}
+public int getState(){
+return this.state;
+}
+public void setZipcode(String zipcode){
+this.zipcode=zipcode;
+}
+public String getZipcode(){
+return this.zipcode;
+}
+public void setCountry(int country){
+this.country=country;
+}
+public int getCountry(){
+return this.country;
+}
+
+}
---
+++ cms/src/com/cms/employee/dao/UserMasterDAO.java
@@ -0,0 +1,159 @@
+package com.cms.employee.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.cms.common.db.connection.DBConnection;
+import com.cms.common.db.util.DBUtil;
+import com.cms.employee.bean.UserMasterDO;
+
+public class UserMasterDAO {
+
+private static final String SELECT="select   emp_id, first_name, last_name, email, gender, marital_status, blood_group, mobile, date_of_birth, department, role, profile, reporting_manager, subordinates, pan_card, bank_detials, epf_no, street, city, state, zipcode, country from user_master ";
+private static final String INSERT="insert into user_master( emp_id, first_name, last_name, email, gender, marital_status, blood_group, mobile, date_of_birth, department, role, profile, reporting_manager, subordinates, pan_card, bank_detials, epf_no, street, city, state, zipcode, country)  values(  ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? ) ";
+private static final String UPDATE="update  user_master set  first_name=?, last_name=?, email=?, gender=?, marital_status=?, blood_group=?, mobile=?, date_of_birth=?, department=?, role=?, profile=?, reporting_manager=?, subordinates=?, pan_card=?, bank_detials=?, epf_no=?, street=?, city=?, state=?, zipcode=?, country=? WHERE emp_id=? ";
+
+public static int insert(Connection preCon, UserMasterDO dto) {
+int insertId=0;
+Connection con=null;
+PreparedStatement stmt=null;
+ResultSet rs=null;
+try {
+con=preCon==null?DBConnection.getConnection():preCon;
+stmt=con.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
+int i=1;
+stmt.setInt(i++, dto.getEmpId() );
+stmt.setString(i++, dto.getFirstName() );
+stmt.setString(i++, dto.getLastName() );
+stmt.setString(i++, dto.getEmail() );
+stmt.setString(i++, dto.getGender() );
+stmt.setString(i++, dto.getMaritalStatus() );
+stmt.setInt(i++, dto.getBloodGroup() );
+stmt.setInt(i++, dto.getMobile() );
+stmt.setString(i++, dto.getDateOfBirth() );
+stmt.setInt(i++, dto.getDepartment() );
+stmt.setInt(i++, dto.getRole() );
+stmt.setInt(i++, dto.getProfile() );
+stmt.setInt(i++, dto.getReportingManager() );
+stmt.setString(i++, dto.getSubordinates() );
+stmt.setString(i++, dto.getPanCard() );
+stmt.setString(i++, dto.getBankDetials() );
+stmt.setString(i++, dto.getEpfNo() );
+stmt.setString(i++, dto.getStreet() );
+stmt.setString(i++, dto.getCity() );
+stmt.setInt(i++, dto.getState() );
+stmt.setString(i++, dto.getZipcode() );
+stmt.setInt(i++, dto.getCountry() );
+stmt.execute();
+rs=stmt.getGeneratedKeys();
+if(rs.next()) { insertId=rs.getInt(1); }
+} catch (Exception e) { e.printStackTrace(); }
+finally { DBUtil.close( rs, stmt, preCon==null?con:null); }
+return insertId;
+}
+
+public static boolean update(Connection preCon, UserMasterDO dto) {
+Connection con=null;
+PreparedStatement stmt=null;
+int i=1;
+try {
+con=preCon==null?DBConnection.getConnection():preCon;
+stmt=con.prepareStatement(UPDATE);
+stmt.setString(i++, dto.getFirstName());
+stmt.setString(i++, dto.getLastName());
+stmt.setString(i++, dto.getEmail());
+stmt.setString(i++, dto.getGender());
+stmt.setString(i++, dto.getMaritalStatus());
+stmt.setInt(i++, dto.getBloodGroup());
+stmt.setInt(i++, dto.getMobile());
+stmt.setString(i++, dto.getDateOfBirth());
+stmt.setInt(i++, dto.getDepartment());
+stmt.setInt(i++, dto.getRole());
+stmt.setInt(i++, dto.getProfile());
+stmt.setInt(i++, dto.getReportingManager());
+stmt.setString(i++, dto.getSubordinates());
+stmt.setString(i++, dto.getPanCard());
+stmt.setString(i++, dto.getBankDetials());
+stmt.setString(i++, dto.getEpfNo());
+stmt.setString(i++, dto.getStreet());
+stmt.setString(i++, dto.getCity());
+stmt.setInt(i++, dto.getState());
+stmt.setString(i++, dto.getZipcode());
+stmt.setInt(i++, dto.getCountry());
+stmt.setInt(i++,dto.getEmpId());
+int rowAffect=stmt.executeUpdate();
+if(rowAffect!=0) { return true; }
+} catch (Exception e) { e.printStackTrace(); } 
+finally { DBUtil.close( stmt, preCon==null?con:null  ); }
+return false;
+}
+
+public static List<UserMasterDO> getUserMaster(Connection preCon, boolean needChild) {
+String query=SELECT;
+List<UserMasterDO> dtoList =getUserMaster(preCon, query, needChild);
+if( dtoList==null ) { dtoList=new ArrayList<UserMasterDO>(); }
+return dtoList;
+}
+
+public static UserMasterDO getUserMasterByEmpId(Connection preCon, int empId, boolean needChild) {
+String query=SELECT+" WHERE emp_id="+empId;
+List<UserMasterDO> dtoList =getUserMaster(preCon, query, needChild);
+if( dtoList==null ) { dtoList=new ArrayList<UserMasterDO>(); }
+return dtoList.size()>0?dtoList.get(0): new UserMasterDO();
+}
+
+private  static List<UserMasterDO> getUserMaster(Connection preCon, String query, boolean needChild) {
+List<UserMasterDO> dtos=new ArrayList<UserMasterDO>();
+Connection con=null;
+Statement stmt=null;
+ResultSet rs=null;
+try {
+con=preCon==null?DBConnection.getConnection():preCon;
+stmt=con.createStatement();
+rs=stmt.executeQuery( query );
+while(rs.next()) { dtos.add(constructDTO( con, rs, needChild) );	}
+} catch (Exception e) { e.printStackTrace(); }
+finally { DBUtil.close( rs, stmt, preCon==null?con:null ); }
+return dtos;
+}
+
+private static UserMasterDO constructDTO( Connection con, ResultSet rs, boolean needChild ) {
+UserMasterDO dto=new UserMasterDO();
+try {
+int i=1;
+dto.setEmpId(rs.getInt(i++));
+dto.setFirstName(rs.getString(i++));
+dto.setLastName(rs.getString(i++));
+dto.setEmail(rs.getString(i++));
+dto.setGender(rs.getString(i++));
+dto.setMaritalStatus(rs.getString(i++));
+dto.setBloodGroup(rs.getInt(i++));
+dto.setMobile(rs.getInt(i++));
+dto.setDateOfBirth(rs.getString(i++));
+dto.setDepartment(rs.getInt(i++));
+dto.setRole(rs.getInt(i++));
+dto.setProfile(rs.getInt(i++));
+dto.setReportingManager(rs.getInt(i++));
+dto.setSubordinates(rs.getString(i++));
+dto.setPanCard(rs.getString(i++));
+dto.setBankDetials(rs.getString(i++));
+dto.setEpfNo(rs.getString(i++));
+dto.setStreet(rs.getString(i++));
+dto.setCity(rs.getString(i++));
+dto.setState(rs.getInt(i++));
+dto.setZipcode(rs.getString(i++));
+dto.setCountry(rs.getInt(i++));
+} catch (SQLException e) { e.printStackTrace(); }
+finally { }
+return dto;
+} 
+
+
+}
